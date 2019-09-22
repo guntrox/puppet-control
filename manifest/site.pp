8206ea99d8251eeeea37b8dd64c7e72a44308167
Olwyn Meek <ojmeek@yahoo.com>
2019-09-21 21:02:50 -0400

    added a vagrant machine and updates puppetmaster manifests

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,3 +1,8 @@
 node default {
-
+  if $facts[role] {
+    include "role::${facts[role]}"
+  } else {
+    notify {"Node ${facts[fqdn]} does not have a role set specified":}
+    include profile::base
+  }
 }
--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -5,4 +5,6 @@
 # @example
 #   include profile::puppetmaster
 class profile::puppetmaster {
+  class { 'puppetserver::repository': }
+    -> class { 'puppetserver': }
 }
